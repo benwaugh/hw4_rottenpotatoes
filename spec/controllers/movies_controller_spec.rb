712d2645fca8f768918ab2cc744c77f3bab305e7
Ben Waugh <b.waugh@ucl.ac.uk>
2012-07-02 01:47:52 -0700

    Spec to sort by date.

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 describe MoviesController do
-  describe 'finding similar movies' do
+  describe 'adding new movie' do
     it 'should create a new movie' do
       post :create
+    end
+  end
+  describe 'sorting movies' do
+    it 'should sort by title' do
+      get :index, {:sort => 'title'}
     end
   end
 end
