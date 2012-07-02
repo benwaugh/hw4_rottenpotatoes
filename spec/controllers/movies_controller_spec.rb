30df1da0a1e85f9ebf03f0e1d2c3da9bc1d2f37f
Ben Waugh <b.waugh@ucl.ac.uk>
2012-07-02 01:05:27 -0700

    Simplify movie controller spec so it doesn't create any errors.

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe MoviesController do
   describe 'finding similar movies' do
     it 'should create a new movie' do
+      Movie.should_receive(:create)
       post :create
     end
   end
